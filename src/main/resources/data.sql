d79c6034baa4caaf635dbc0569c265641bf8ede3
ydiouy <ydiouy@gmail.com>
2025-04-25 22:43:35 +0200

    Fix database properties variables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,6 @@
--- CREATE DATABASE pay_my_buddy;
--- \c pay_my_buddy
+-- This file is used to initialize the database with some data.
 
-DROP TABLE IF EXISTS "transaction";
-DROP TABLE IF EXISTS "connection_user";
-DROP TABLE IF EXISTS "user";
+--\c pay_my_buddy
 
 CREATE TABLE IF NOT EXISTS "user" (
     id SERIAL NOT NULL PRIMARY KEY,
@@ -28,20 +25,6 @@ CREATE TABLE IF NOT EXISTS transaction (
     date DATE NOT NULL
 );
 
-INSERT INTO "user" (username, email, password)
-VALUES
-    ('jdoe', 'jdoe@email.com', '$2a$12$Y2j1Yoj8x3Do6.JPXJKd3ucj2Iy4nZzptytPUJEsZSNr6MOGS8AhK'),
-    ('janedoe', 'janedoe@email.com', '$2a$12$yvriMy7O4AvwNuMCTUYJ..aNFo8yWSQqnWeKngjlIuTpbP7CKaLky'),
-    ('luc', 'luc@email.com', '$2a$12$kn/jQe4gOLpH1dnZYVgI3.MCS8Oj1LXnbh82jZhO02TE8oqpLZsgu'),
-    ('clara', 'clara@email.com', '$2a$12$jr6VdWeOHCmsv9G4Rt/6O.WpU7.eaDPtQ8eSYVemLwYDjuM6Oaclm'),
-    ('anne', 'anne@email.com', '$2a$12$BDsRp6pzxueCV58eBAQoHOzfNa.O2VV4xOPqpiq56oF8g6hX2AGeq');
-
-INSERT INTO connection_user (user_id, associated_user_id, date)
-VALUES (1, 2, CURRENT_DATE);
-
-INSERT INTO transaction (sender_user_id, receiver_user_id, description, amount, date)
-VALUES (1, 2, 'Ma description', '2400.69', NOW());
-
 ALTER TABLE IF EXISTS transaction
     ADD FOREIGN KEY (sender_user_id) REFERENCES "user"(id);
 
@@ -53,3 +36,18 @@ ALTER TABLE IF EXISTS connection_user
 
 ALTER TABLE IF EXISTS connection_user
     ADD FOREIGN KEY (associated_user_id) REFERENCES "user"(id);
+
+INSERT INTO "user" (username, email, password)
+VALUES
+    ('jdoe', 'jdoe@email.com', '$2a$12$Y2j1Yoj8x3Do6.JPXJKd3ucj2Iy4nZzptytPUJEsZSNr6MOGS8AhK'),
+    ('janedoe', 'janedoe@email.com', '$2a$12$yvriMy7O4AvwNuMCTUYJ..aNFo8yWSQqnWeKngjlIuTpbP7CKaLky'),
+    ('luc', 'luc@email.com', '$2a$12$kn/jQe4gOLpH1dnZYVgI3.MCS8Oj1LXnbh82jZhO02TE8oqpLZsgu'),
+    ('clara', 'clara@email.com', '$2a$12$jr6VdWeOHCmsv9G4Rt/6O.WpU7.eaDPtQ8eSYVemLwYDjuM6Oaclm'),
+    ('anne', 'anne@email.com', '$2a$12$BDsRp6pzxueCV58eBAQoHOzfNa.O2VV4xOPqpiq56oF8g6hX2AGeq')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO connection_user (user_id, associated_user_id, date)
+VALUES (1, 2, CURRENT_DATE) ON CONFLICT DO NOTHING;
+
+INSERT INTO transaction (sender_user_id, receiver_user_id, description, amount, date)
+VALUES (1, 2, 'Ma description', '2400.69', NOW()) ON CONFLICT DO NOTHING;
---
+++ src/main/resources/init_database.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE pay_my_buddy;
+\c pay_my_buddy
