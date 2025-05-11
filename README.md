cb44196339baa3d7270f508dde6da2928fdb9930
ydiouy <ydiouy@gmail.com>
2025-05-11 16:38:52 +0200

    Move sql to resources directory

--- README.md
+++ README.md
@@ -1,30 +1,25 @@
 # pay_my_buddy
 
-| Tables            | Columns                                                                      |
-|-------------------|------------------------------------------------------------------------------|
-| users             | (PK) id INT, username VARCHAR(50), email VARCHAR(255), password VARCHAR(255) |
-| association_users | (PK) id INT, (FK) user_id INT, (FK) associated_user_id INT                   |
-| transactions      | (PK) id INT, (FK) user_id_1 INT, (FK) user_id_2 INT, amount DECIMAL          |
+| Tables            | Columns                                                                            |
+|-------------------|------------------------------------------------------------------------------------|
+| users             | (PK) id INT, username VARCHAR(50), email VARCHAR(255), password VARCHAR(255)       |
+| association_users | (PK) id INT, (FK) user_id INT, (FK) associated_user_id INT, date DATETIME          |
+| transactions      | (PK) id INT, (FK) user_id_1 INT, (FK) user_id_2 INT, amount DECIMAL, date DATETIME |
 
 Initialisation de la base de données :
+
 ```
-psql -h localhost -p 5432 -U postgres -f ./src/main/resources/create_database.sql
-psql -h localhost -p 5432 -U postgres -d pay_my_buddy -f ./src/main/resources/data.sql
-psql -h localhost -p 5432 -U postgres -d pay_my_buddy -f ./src/main/resources/create_trigger_function_user_order.sql
+psql -h localhost -p 5432 -U postgres -f ./resources/create_database.sql
+psql -h localhost -p 5432 -U postgres -d pay_my_buddy -f ./resources/data.sql
 
 # Initialisation de la base de données de test
 psql -h localhost -p 5432 -U postgres -f ./src/test/resources/create_database_test.sql
 psql -h localhost -p 5432 -U postgres -d pay_my_buddy_test -f ./src/test/resources/data-test.sql
 psql -h localhost -p 5432 -U postgres -d pay_my_buddy_test -f ./src/main/resources/create_trigger_function_user_order.sql
 ```
+
 Vérification de l'initialisation
+
 ```
 psql -h localhost -p 5432 -U postgres -d pay_my_buddy -c "\dt"
-```
-
-Exemple de configuration de la base de données dans le fichier `application.properties` :
-```
-spring.datasource.url = jdbc:postgresql://localhost:8888/nom_de_la_base
-spring.datasource.username = username
-spring.datasource.password = password
 ```
--- resources/data.sql
+++ resources/data.sql
@@ -1,5 +1,4 @@
--- This file is used to initialize the database with some data.
-
+-- It contains the SQL commands to create the tables.
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
@@ -20,7 +19,7 @@ CREATE TABLE IF NOT EXISTS transactions (
     receiver_user_id INTEGER NOT NULL,
     description TEXT null,
     amount DECIMAL(10, 2) NOT NULL,
-    date DATE NOT NULL
+    date DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
 ALTER TABLE IF EXISTS transactions
@@ -39,8 +38,29 @@ ALTER TABLE IF EXISTS connection_users
     ADD FOREIGN KEY (user_id_1) REFERENCES "users"(id),
     ADD FOREIGN KEY (user_id_2) REFERENCES "users"(id),
     ADD CONSTRAINT chk_connection_user_id_different
-        CHECK (user_id_1 <> user_id_2);
+        CHECK (user_id_1 <> user_id_2),
+    ADD CONSTRAINT chk_ordered_user_id
+        CHECK (user_id_1 < user_id_2);
 
+-- The following function and trigger ensure that the user IDs in the connection_users table are always ordered.
+CREATE OR REPLACE FUNCTION enforce_user_id_connection()
+    RETURNS TRIGGER AS $_func$
+DECLARE
+    initial_user_id_1 INTEGER;
+BEGIN
+    initial_user_id_1 := NEW.user_id_1;
+    NEW.user_id_1 := LEAST(NEW.user_id_1, NEW.user_id_2);
+    NEW.user_id_2 := GREATEST(initial_user_id_1, NEW.user_id_2);
+    RETURN NEW;
+END;
+$_func$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS trigger_user_connection ON connection_users;
+CREATE TRIGGER trigger_user_connection BEFORE INSERT OR UPDATE ON connection_users
+    FOR EACH ROW
+EXECUTE FUNCTION enforce_user_id_connection();
+
+-- The following SQL commands insert sample data into the users and connection_users tables.
 INSERT INTO "users" (username, email, password)
     VALUES
         ('jdoe', 'jdoe@email.com', '$2a$12$Y2j1Yoj8x3Do6.JPXJKd3ucj2Iy4nZzptytPUJEsZSNr6MOGS8AhK'),
@@ -50,9 +70,9 @@ INSERT INTO "users" (username, email, password)
         ('anne', 'anne@email.com', '$2a$12$BDsRp6pzxueCV58eBAQoHOzfNa.O2VV4xOPqpiq56oF8g6hX2AGeq')
 ON CONFLICT DO NOTHING;
 
-INSERT INTO connection_users (user_id_1, user_id_2, date)
-VALUES (1, 2, CURRENT_DATE)
+INSERT INTO connection_users (user_id_1, user_id_2)
+VALUES (1, 2)
 ON CONFLICT DO NOTHING;
 
--- INSERT INTO transaction (sender_user_id, receiver_user_id, description, amount, date)
--- VALUES (1, 2, 'Ma description', '2400.69', NOW()) ON CONFLICT DO NOTHING;
+INSERT INTO transactions (sender_user_id, receiver_user_id, description, amount)
+VALUES (1, 2, 'Ma description', '2400.69') ON CONFLICT DO NOTHING;
